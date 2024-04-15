15feb948e8e0a1995bfb51fdc17d00c0b4ceb183
Alexey Kucherenko <killmenot@users.noreply.github.com>
2024-04-15 20:57:35 +0300

    chore: add git clean scripts (#3)

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,5 @@ const test = (r) => r.test.bind(r);
 module.exports = {
   defaultIgnores: false,
   extends: ['@commitlint/config-conventional'],
-  ignores: [
-    test(/Merge branch (.*?)(in|into) (.*)/),
-  ],
+  ignores: [test(/Merge branch (.*?)(in|into) (.*)/)],
 };
