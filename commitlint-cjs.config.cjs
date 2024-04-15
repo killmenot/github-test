509f3bce17c64415a72c1af21625188ccde2563f
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2024-04-15 20:31:19 +0300

    ci: test fixes

--- commitlint-cjs.config.cjs
+++
@@ -1,7 +0,0 @@
-const test = (r) => r.test.bind(r);
-
-module.exports = {
-  defaultIgnores: false,
-  extends: ['@commitlint/config-conventional'],
-  ignores: [test(/Merge branch (.*?)(in|into) (.*)/)],
-};
