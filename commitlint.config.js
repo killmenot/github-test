dbad98717539609942f5ba9382421eb13a5387b1
Alex <167097806+aspecsupermario@users.noreply.github.com>
2024-04-15 19:26:10 +0300

    ci: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+const test = (r) => r.test.bind(r);
+
 module.exports = {
+  defaultIgnores: false,
   extends: ['@commitlint/config-conventional'],
+  ignores: [
+    test(/Merge branch (.*?)(in|into) (.*)/),
+  ],
 };
